2f8efe40f78fdec8de09e1ffa9624a8430968967
Anuj <anashkan123@gmail.com>
2015-06-16 14:19:59 +0000

    Commiting The Static Pages And Navigating Routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     #assert_select "title", "About | Ruby on rails Tutorial sample app"
   end
   
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    #assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
+  
 end
