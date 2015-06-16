fdddd1119074f75edef74d1723b699f1b5482860
Anuj <anashkan123@gmail.com>
2015-06-16 03:47:02 +0000

    Adding Introduction Route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def intro
+    render text: "Ruby On Rails Is Trending!!!"
+  end
+  
 end
+
