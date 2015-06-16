32763be5818c04f8d0b33c0cec62442a90670db2
Anuj <anashkan123@gmail.com>
2015-06-16 04:56:29 +0000

    Finishing static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def intro
-    render text: "Ruby On Rails Is Trending!!!"
-  end
+  
   
 end
 
